11730842855b6cf31fc8a271295708d404cc0f43
“huailei000” <2280131253@qq.com>
2022-04-24 15:25:13 +0800

    feat: tabs支持拖拽

--- src/app/elements/content-tab/content-tab.component.ts
+++ src/app/elements/content-tab/content-tab.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
 import {View, ViewAction} from '@app/model';
+import * as jQuery from 'jquery/dist/jquery.min.js';
 
 @Component({
   selector: 'elements-content-tab',
@@ -10,6 +11,9 @@ export class ElementContentTabComponent implements OnInit {
   @Input() view: View;
   @Output() onAction: EventEmitter<ViewAction> = new EventEmitter<ViewAction>();
   public iconCls: string;
+
+  isDragging = false;
+  isDragover = false;
 
   ngOnInit(): void {
     if (!this.view.node) {
@@ -27,5 +31,46 @@ export class ElementContentTabComponent implements OnInit {
   setActive() {
     const action = new ViewAction(this.view, 'active');
     this.onAction.emit(action);
+  }
+
+  // 在元素开始被拖动时候触发
+  dragstart(event) {
+    this.isDragging = true;
+    event.dataTransfer.setData('Text', event.target.id);
+  }
+  // 在拖动操作完成时触发
+    dragend(event) {
+    this.isDragging = false;
+  }
+  // 当被拖动元素在目的地元素内时触发
+    dragover(e) {
+    const dom = document.getElementById(e.target.id);
+    e.preventDefault();
+    this.isDragover = true;
+  }
+  // 当被拖动元素没有放下就离开目的地元素时触发
+  dragleave(event) {
+    this.isDragover = false;
+  }
+
+  drop(e) {
+    this.isDragging = false;
+    this.isDragover = false;
+    const data = e.dataTransfer.getData('Text');
+    const dom = document.getElementById(data);
+    const cur = document.getElementById(e.currentTarget.id);
+    // this.temp = e.currentTarget.innerHTML;
+    // e.currentTarget.innerHTML = dom.innerHTML;
+    // dom.innerHTML = this.temp;
+
+    let startLen = cur.children.length;
+    let endLen = dom.children.length;
+    for (; startLen--;) {
+      dom.appendChild(cur.children[0]);
+    }
+    for (; endLen--;) {
+      cur.appendChild(dom.children[0]);
+    }
+    e.preventDefault();
   }
 }
